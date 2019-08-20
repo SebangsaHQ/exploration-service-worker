ffe227c95de36c05b17792dd1e279e7caf298ee8
yussan <yusuf@sebangsa.com>
2019-08-20 17:31:56 +0700

    ready to build

--- 3.0.background-sync/service-worker.js
+++ 3.0.background-sync/service-worker.js
@@ -1,7 +1,8 @@
 self.addEventListener("sync", function(event) {
+  console.log("event", event)
   if(event.tag == "sync-submit-forgot-password") {
     event.waitUntil(function(){
-      console.log("self")
+      console.log("iam ready")
     })
   }
 })
